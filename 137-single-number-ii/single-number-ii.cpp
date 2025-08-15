7fdccfa84858c201b6e981fc46743cdbe5099f4c
Yuvraj Singh <singhyuvi4321@gmail.com>
2025-08-15 14:47:36 +0530

    Time: 3 ms (32.64%) | Memory: 14.1 MB (11.10%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        map<int,int> m;
+        for(int i = 0; i < nums.size(); i++) {
+            m[nums[i]]++;
+        }
+        for(auto it : m) {
+            if(it.second == 1) return it.first;
+        }
+        return -1; // In case no unique number exists
+    }
+};
