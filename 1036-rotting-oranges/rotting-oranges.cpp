a7c45208b9a65f859f3dba3aaf4fd9a289231f1d
Yuvraj Singh <singhyuvi4321@gmail.com>
2025-08-26 13:06:40 +0530

    Time: 0 ms (100.00%) | Memory: 16.9 MB (55.47%) - LeetSync

---
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        queue<pair<pair<int,int>,int>> q;
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+
+        // Step 1: Push all initially rotten oranges into queue
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==2){
+                    q.push({{i,j},0});
+                    vis[i][j]=2;  // mark as rotten
+                }
+                else vis[i][j]=0;
+            }
+        }
+
+        int tm = 0;
+        int drow[] = {-1,0,+1,0};
+        int dcol[] = {0,1,0,-1};
+
+        // Step 2: BFS
+        while(!q.empty()){
+            int r = q.front().first.first;
+            int c = q.front().first.second;
+            int t = q.front().second;
+            q.pop();
+
+            tm = max(tm, t);  // track max time
+
+            for(int i=0;i<4;i++){
+                int nrow = r+drow[i];
+                int ncol = c+dcol[i];
+
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m 
+                   && vis[nrow][ncol] !=2 && grid[nrow][ncol]==1){
+                    q.push({{nrow,ncol},t+1});
+                    vis[nrow][ncol] = 2;  // mark as rotten
+                }
+            }
+        }
+
+        // Step 3: Check if any fresh orange remains
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(vis[i][j]!=2 && grid[i][j]==1){
+                    return -1;
+                }
+            }
+        }
+        return tm;
+    }
+};
